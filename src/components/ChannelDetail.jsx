58ad55e5705e42542451d97d079db50492a64874
Jan <janvilaaregut@gmail.com>
2022-10-11 19:16:21 +0200

    watch video and channel details

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
-  return <div></div>;
+  const { id } = useParams();
+
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]));
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items));
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            backgroundImage:
+              "radial-gradient(circle at center center, #621c76, #533bef), repeating-radial-gradient(circle at center center, #621c76, #621c76, 32px, transparent 64px, transparent 32px",
+            backgroundBlendMode: "multiply",
+            backgroundColor: "#533bef",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
